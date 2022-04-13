c1e82fc8d8d0a622319ca71d75e8fe08babcaa3b
Narek Yazeryan <yazeryann@gmail.com>
2022-04-13 22:15:53 +0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Button from './components/Button/Button';
-import Checkbox from './components/Checkbox/Checkbox';
 import React from 'react'
 
 function App() {
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-
-  const checkboxClickHandler = () => {
-    setButtonDisabled( !buttonDisabled );
-  }
   return (
     <div className="App">
-      <Button disabled={buttonDisabled} />
-      <Checkbox checked={buttonDisabled} onChange={checkboxClickHandler} title="Disable the button" />
+      <h2>Hello React</h2>
     </div>
   );
 }
