d2de33a99967034f56488eb64350568ef588bae4
Narek Yazeryan <yazeryann@gmail.com>
2022-04-28 15:50:38 +0400

    Fixing Issue with React lazy components

--- src/pages/Entry/Options/Options.tsx
+++ src/pages/Entry/Options/Options.tsx
@@ -1,24 +1,29 @@
+import React, { Suspense } from 'react';
 import { Row } from 'react-bootstrap';
 import { OptionType } from '../../../Types/Main';
 import { useOptions } from './hooks/useOptions';
-import ScoopOption from './ScoopOption';
-import React from 'react';
-import ToppingOption from './ToppingOption';
-import AlertBox from '../../../components/Alert';
+
+// Lazy Components
+const ScoopOption = React.lazy(() => import('./ScoopOption'));
+const ToppingOption = React.lazy(() => import('./ToppingOption'));
+const AlertBox = React.lazy(() => import('../../../components/Alert'));
 
 interface Props {
   type: OptionType;
   [x: string]: unknown;
 }
+
 const Options = ({type, ...restProps}: Props) => {
   const [items, error] = useOptions(type);
-
+  
   const ItemComponent = type === 'scoops' ? ScoopOption : ToppingOption;
   const renderItems = () => items.map( item => <ItemComponent name={item.name} imagePath={item.imagePath} key={item.name} /> );
 
   return (
     <Row {...restProps}>
-      {!error ? renderItems() :<AlertBox variant="danger" /> }
+      <Suspense fallback="<span>Loading</span>">
+        {!error ? renderItems() : <AlertBox variant="danger" /> }
+      </Suspense>
     </Row>
   );
 };
