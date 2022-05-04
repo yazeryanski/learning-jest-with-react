7f7760817f812c510e30c726016de75fe077a822
Narek Yazeryan <yazeryann@gmail.com>
2022-05-04 21:50:24 +0400

    Removing Console.log

--- src/contexts/Order/helpers.ts
+++ src/contexts/Order/helpers.ts
@@ -6,7 +6,7 @@ import { OptionCountType } from './Types';
 
 export function calculateSubtotal(optionType: OptionType, optionCounts: OptionCountType) {
   let optionCount = 0;
-  console.log( optionCounts[optionType].values() );
+
   for (const count of optionCounts[optionType].values()) {
     optionCount += count;
   }
